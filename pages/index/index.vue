284ef4b0fb15dfa923c73ec71b1fc21a47e6a26f
HoraceHYY <horacehuang17@gmail.com>
2022-07-08 17:13:36 +0800

    Initial Commit

---
+++ pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="gender-select">
+		<text class="gender-title">请选择性别</text>
+		<radio-group class="radios">
+			<radio style="padding: 20rpx;">男</radio>
+			<radio style="padding: 20rpx;">女</radio>
+		</radio-group>
+		<button class="next-button">下一步</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	view {
+		display: flex;
+	}
+	
+	.gender-select {
+		flex-direction: column;
+	}
+	
+	.gender-title {
+		margin-top: 200rpx;
+		text-align: center;
+		font-size: 40rpx;
+		font-weight: 700;
+	}
+	
+	.radios {
+		align-self: center;
+		justify-items: center;
+	}
+	
+	.next-button {
+		margin-top: 20rpx;
+		font-size: 10rpx;
+		background-color: #4cb6e8;
+		border: none;
+		color: #eff1fe;
+	}
+</style>
---
+++ pages/mainPage/mainPage.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/human-man-scan-190m-01.jpg">
+			<image @click="arm_click" class="circle-button" style="position: absolute; top: 200rpx; left: 230rpx;" src="../../static/circle-button.png"/>
+			<image @click="body_click" class="circle-button" style="position: absolute; top: 240rpx; left: 350rpx;" src="../../static/circle-button.png"/>
+			<image @click="leg_click" class="circle-button" style="position: absolute; top: 550rpx; left: 300rpx;" src="../../static/circle-button.png"/>
+		</image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			arm_click() {
+				
+			},
+			body_click() {
+				
+			},
+			leg_click() {
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: #f2f2f2;
+	}
+	
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		width: 800rpx;
+		height: 800rpx;
+		align-self: center;
+	}
+	
+	.circle-button {
+		width: 50rpx;
+		height: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+</style>
